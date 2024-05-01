a749a86d86ed36cee943a0c79e8071d1c0beb05f
shouta0715 <Myheroes0715@gmail.com>
2024-05-01 20:40:52 +0900

    add: basic components

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref as R } from 'vue';
 import basic from '@/components/basic.vue';
 import ref from '@/components/ref.vue';
 import computed from '@/components/computed.vue';
@@ -8,6 +9,24 @@ import On from '@/components/on.vue';
 import Form from '@/components/form.vue';
 import lifeCycle from '@/components/life-cycle.vue';
 import watch from '@/components/watch.vue';
+import Comp from '@/components/component.vue';
+
+const components = {
+  ref,
+  computed,
+  ifComp,
+  List,
+  On,
+  Form,
+  lifeCycle,
+  watch,
+  Comp,
+};
+
+const currentComponent = R<keyof typeof components>('ref');
+
+const titles = R<string[]>(['t1', 't2', 't3']);
+const fontSize = R(16);
 </script>
 
 <template>
@@ -15,11 +34,44 @@ import watch from '@/components/watch.vue';
     <basic msg="Vite + Vue" />
     <ref />
     <computed />
-    <ifComp />
+    <if-comp />
     <List />
     <On />
     <Form />
     <lifeCycle />
     <watch />
+    <Comp title="t" />
+
+    <div :style="{ fontSize: fontSize + 'px' }">
+      <Comp
+        v-for="title in titles"
+        :title="title"
+        :key="title"
+        @enlarge="fontSize += 2"
+      >
+        <p>{{ title + 'paragraph' }}</p>
+      </Comp>
+    </div>
+
+    <div>
+      <div class="flex gap-8 flex-wrap">
+        <button
+          v-for="(_, component) in components"
+          :key="component"
+          @click="currentComponent = component"
+          :class="[
+            'p-2 rounded',
+            {
+              'bg-blue-500': currentComponent === component,
+              'bg-gray-300': currentComponent !== component,
+              'text-white': currentComponent === component,
+            },
+          ]"
+        >
+          {{ component }}
+        </button>
+      </div>
+      <component :is="components[currentComponent]" />
+    </div>
   </div>
 </template>
---
+++ src/components/component.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { title } = defineProps<{ title: string }>();
+defineEmits(['enlarge']);
+</script>
+
+<template>
+  <h4>
+    {{ title }}
+  </h4>
+  <button @click="$emit('enlarge')">Click me</button>
+
+  <p>
+    <slot />
+  </p>
+</template>
